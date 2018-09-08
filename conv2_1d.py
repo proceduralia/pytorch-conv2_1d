23a9354bd6ce7425593ef78228c2ffc128351201
proceduralia <pierluca.doro@gmail.com>
2018-09-08 15:00:09 +0200

    added transposed convolution

--- conv2_1d.py
+++ conv2_1d.py
@@ -35,3 +35,38 @@ class Conv2_1d(nn.Module):
         x = x.view(b, dr1, dr2, out_c, out_t)
         x = x.permute(0, 3, 4, 1, 2).contiguous()
         return x
+
+class ConvTranspose2_1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=(1, 1, 1),
+                        padding=(0, 0, 0), dilation=(1, 1, 1), bias=True):
+        super().__init__()
+        t = kernel_size[0]
+        d = (kernel_size[1] + kernel_size[2])//2
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        #Hidden size estimation to get a number of parameter similar to the 3d case
+        self.hidden_size = int((t*d**2*in_channels*out_channels)/(d**2*in_channels+t*out_channels))
+
+        self.convTranspose2d = nn.ConvTranspose2d(in_channels, self.hidden_size, kernel_size[1:], stride[1:], padding[1:], bias=bias)
+        self.convTranspose1d = nn.ConvTranspose1d(self.hidden_size, out_channels, kernel_size[0], stride[0], padding[0], bias=bias)
+
+    def forward(self, x):
+        #2D convolution
+        b, c, t, d1, d2 = x.size()
+        x = x.permute(0,2,1,3,4).contiguous()
+        x = x.view(b*t, c, d1, d2)
+        x = F.relu(self.convTranspose2d(x))
+        
+        #1D convolution
+        c, dr1, dr2 = x.size(1), x.size(2), x.size(3)
+        x = x.view(b, t, c, dr1, dr2)
+        x = x.permute(0, 3, 4, 2, 1).contiguous()
+        x = x.view(b*dr1*dr2, c, t)
+        x = self.convTranspose1d(x)
+
+        #Final output
+        out_c, out_t = x.size(1), x.size(2)
+        x = x.view(b, dr1, dr2, out_c, out_t)
+        x = x.permute(0, 3, 4, 1, 2).contiguous()
+        return x
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from torch.autograd import Variable
 import torch.nn as nn
 import torch
-from conv2_1d import Conv2_1d
+from conv2_1d import Conv2_1d, ConvTranspose2_1d
 
-class Model(nn.Module):
+class DownConvModel(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -28,15 +28,47 @@ class Model(nn.Module):
         out = out.view(x.size(0), -1)
         return out
 
+class UpConvModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+              ConvTranspose2_1d(1, 64, (4,4,4), (2,2,2), (1,1,1)),
+              nn.LeakyReLU(inplace=True),
+
+              ConvTranspose2_1d(64, 128, (4,4,4), (2,2,2), (1,1,1)),
+              nn.LeakyReLU(inplace=True),
+
+              ConvTranspose2_1d(128, 3, (2,4,4), (1,1,1), (0,0,0))
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        return out
+
 if __name__ == "__main__":
         seq_size = 32
-        batch_size = 16
+        batch_size = 8
         channel_size = 3
         spatial_size = 64
 
+        print("Testing convolution...")
         input_ = torch.rand(batch_size, channel_size, seq_size, spatial_size, spatial_size)
         input_ = Variable(input_)
         print("Created input of size:", input_.size())
-        d = Model()
+        d = DownConvModel()
+        out = d(input_)
+        print("Output size:", out.size())
+
+
+        seq_size = 2
+        channel_size = 1
+        spatial_size = 4
+        
+        print("Testing transposed convolution...")
+        input_ = torch.rand(batch_size, channel_size, seq_size, spatial_size, spatial_size)
+        input_ = Variable(input_)
+        print("Created input of size:", input_.size())
+        d = UpConvModel()
         out = d(input_)
         print("Output size:", out.size())
